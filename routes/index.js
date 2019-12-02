ec4f19ca78f53549c3511f2fcc332ce89ffc4101
Mateo <mateo_93_@hotmail.com>
2019-12-02 18:36:49 -0500

    mit4

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,5 @@ const express = require('express')
 const scraper = require('../controllers/scraper')
 const api = express.Router()
 
-api.post('/perfil', scraper.perfil)
-api.post('/query', scraper.query)
 api.post('/seguidores', scraper.seguidores)
 module.exports = api
