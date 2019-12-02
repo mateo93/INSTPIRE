130f766b81e3512d85942b82a330319828336000
Mateo <mateo_93_@hotmail.com>
2019-12-02 18:25:21 -0500

    mit4

--- controllers/seguidores.js
+++ controllers/seguidores.js
@@ -1,46 +1,33 @@
 const puppeteer = require('puppeteer');
-const CRED = require('./creds');
-const fs = require("fs");
-const cookies = require("./cookies");
-
-const sleep = async(ms)=>{return new Promise((res, rej)=>{setTimeout(()=>{res();},ms)});}
-const ID = {login:'#id_username',pass:'#id_password'};
-const url='https://www.instagram.com/';
+const cookies = require("./cookies_dc");
+const sleep = async (ms) => {
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      res();
+    }, ms)
+  });
+}
 (async()=>{
-   const browser = await puppeteer.launch({
-    headless: false,
-    devtools: false,
-    args: ['--no-sandbox', '--disable-setuid-sandbox', '--window-size=1200,700'],
-  })
- // const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.setViewport({ width: 1200, height: 700 })
-
-  if(Object.keys(cookies).length)
-   {
-    await page.setCookie(...cookies);
-    await page.goto('https://www.instagram.com/graphql/query/?query_hash=56066f031e6239f35a904ac20c9f37d9&variables={"id":"1233907888","first":50,"after":""}', { waitUntil: 'networkidle2'});
-    const json0 = await page.content();
-    var json1 = json0.replace('<html><head></head><body><pre style="word-wrap: break-word; white-space: pre-wrap;">','');
-    var json = json1.replace('</pre></body></html>','');
-    //console.log(json);
-    //await browser.close();
-   } 
-   else 
-   {
-      let login = async()=>{
-        await page.goto('https://www.instagram.com/accounts/login/?force_classic_login',{ waitUntil:'networkidle2'});
-        await page.waitForSelector(ID.login);
-        await page.type(ID.login, CRED.user);
-        await page.type(ID.pass, CRED.pass);
-        await sleep(500);
-        await page.click(".button-green");
-        await page.waitForNavigation();
-        //await page.goto(url,{ waitUntil:'networkidle2'});
-        let currentCookies = await page.cookies();
-        fs.writeFileSync('./cookies.json', JSON.stringify(currentCookies));
+  try{
+      let instagram = async()=>
+      {
+        const browser = await puppeteer.launch({headless: true,args: ['--no-sandbox', '--disable-setuid-sandbox']});
+        const page = await browser.newPage();
+        await page.setCookie(...cookies);
+        await page.goto('https://www.instagram.com/losmontaneros1/',{ waitUntil:'networkidle2'});
+        // await page.waitForSelector(ID.login);
+        // await page.type(ID.login, CRED.user);
+        // await page.type(ID.pass, CRED.pass);
+        //await sleep(500);
+        await page.waitForSelector(".k9GMp > li > a.-nal3");
+        await page.click(".k9GMp > li > a.-nal3");
+        await page.waitForSelector(".sqdOP.L3NKy.y3zKF");
+        await page.$$eval('button.sqdOP.L3NKy.y3zKF', e=>e.map((button)=>button.click()));  
+        //await page.waitForNavigation();
         await browser.close();
+        console.log('OK');
       }
-      await login();
-   }
+      await instagram();
+    }catch(error){ console.error(error); await browser.close(); }    
 })();
+
